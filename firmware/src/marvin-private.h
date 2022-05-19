62efdd746038246837d5372421e847a4a003bb59
akalamaras <akalamaras1@sheffield.ac.uk>
2022-05-19 12:23:28 +0100

    Added marvin private file

---
+++ firmware/src/marvin-private.h
@@ -0,0 +1,7 @@
+#define _MULTI_SSID1 "NOWTVRCXCG"
+#define _MULTI_KEY1  "ZMvHkSBc9dcT"
+#define COMMAND_RECOGNITION_ACCESS_KEY "YBO4WE2FUPW5ENISALNG674VLRXUSXC5"
+    
+// "YBO4WE2FUPW5ENISALNG674VLRXUSXC5" My Wit.ai key
+// "I3JP3ZA6N2SVC24DNGH2JA7BFGTDQM5F" Hamish's Wit.ai key
+// If you wish to train Marvin using your own voice, set up your Wit.ai key and use that instead
